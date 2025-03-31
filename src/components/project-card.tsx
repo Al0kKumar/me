b2dd0840d77d7105819fd14346d6fcd6bcb2cdcd
Alok <mishraalok189381@gmail.com>
2025-03-31 18:47:41 +0530

    fixed props issue

--- src/components/project-card.tsx
+++ src/components/project-card.tsx
@@ -34,7 +34,6 @@ export function ProjectCard({
   tags,
   link,
   image,
-  video,
   links,
   className,
 }: Props) {
@@ -48,16 +47,7 @@ export function ProjectCard({
         href={href || "#"}
         className={cn("block cursor-pointer", className)}
       >
-        {video && (
-          <video
-            src={video}
-            autoPlay
-            loop
-            muted
-            playsInline
-            className="pointer-events-none mx-auto h-40 w-full object-cover object-top" // needed because random black line at bottom of video
-          />
-        )}
+  
         {image && (
           <Image
             src={image}
